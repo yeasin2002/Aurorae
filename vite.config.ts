9939009895b01cf1836ee38383fe2e02418ec66f
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-01-11 15:02:18 +0600

    chore: updated vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { compression } from 'vite-plugin-compression2'
+import Fonts from 'unplugin-fonts/vite'
+import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), compression(), Fonts({}), svgLoader()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
