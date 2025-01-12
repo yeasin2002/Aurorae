d7f9c3b71594d43812384b207fb10407a2658e30
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-01-12 22:49:23 +0600

    chore: update  shadcn-vue configured  with tailwidcss.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import Button from "~/components/ui/button/Button.vue";
+</script>
+
 <template>
   <div>
-    <h1 class="text-red-800">Hello Form Root</h1>
+    <h1 class="text-red-800">
+      Hello Form Root!!
+      <Button>Click</Button>
+    </h1>
   </div>
 </template>
