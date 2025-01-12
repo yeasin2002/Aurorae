0bfca760e687e928bd6e46d88b7cf0e1f7451f3f
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-01-12 18:59:32 +0600

    refactor: reset to nuxt

--- src/components/LogoFull.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <a class="text-primary font-clash-display">Furniture world </a>
-</template>
--- src/data/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './navigation.data'
-export * from './work-statistics.data'
-export * from './service.data'
-export * from './project-showcase.data'
