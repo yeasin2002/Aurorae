82f46e39dfbed026c7ff2bec25fce504012724b2
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-01-12 22:48:52 +0600

    feat: configured tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: "2025-01-12",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
 });
