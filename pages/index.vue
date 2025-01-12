13ab5d8f023cbffda382eb515e5efb705666bc9f
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-01-13 00:28:27 +0600

    feat: updated metadata and favicon

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,16 @@ import Hero from '~/components/index-page/HeroArea.vue'
 import Service from '~/components/index-page/OurService.vue'
 import Project from '~/components/index-page/ProjectShowCase.vue'
 import Footer from '~/components/common/FooterSection.vue'
+
+useSeoMeta({
+  title: 'Furniture World',
+  ogTitle: 'My Amazing Site',
+  ogDescription: 'Interior design, building  pleasing environment',
+  ogImage: '/cover.png',
+
+  description:
+    'Interior design is the art of science of enhancing the interior of a building to achieve a healthier and more aesthetically pleasing environment for the people using the space.',
+})
 </script>
 
 <template>
