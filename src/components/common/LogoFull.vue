05ebe991d9e0ba2797ec7c193648dc7a4212dfec
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-01-24 01:18:40 +0600

    fix: logo image "Not found"  issues

--- src/components/common/LogoFull.vue
+++ src/components/common/LogoFull.vue
@@ -1,6 +1,6 @@
 <template>
-  <NuxtLink href="/" class="font-clash-display text-primary">
-    <NuxtImage src="/brand/logo-full.svg" alt="Aurorae's Logo" class="size-10" />
+  <NuxtLink href="/" class="font-clash-display text-primary flex gap-x-1 items-center">
+    <NuxtImg src="/brand/logo-large.svg" alt="Aurorae's Logo" class="size-5" />
     <span>Aurorae</span>
   </NuxtLink>
 </template>
